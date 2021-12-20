6aa063674fa47c53d5113b8c5a7f62fa3023b88b
余银海 <591985864@qq.com>
2021-12-20 14:22:43 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 /*
  * @Author: your name
  * @Date: 2021-12-20 10:01:08
- * @LastEditTime: 2021-12-20 12:42:26
- * @LastEditors: your name
+ * @LastEditTime: 2021-12-20 14:22:38
+ * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \react-ui-2\src\index.ts
  */
-export { default as Foo } from './Components/Button';
+export { default as Foo } from './Button';
