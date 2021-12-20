ae208352fa88c94f744ee81b84685d253c4d79a9
余银海 <591985864@qq.com>
2021-12-20 16:01:46 +0800

    update alias

--- .umirc.ts
+++ .umirc.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-12-20 10:01:07
- * @LastEditTime: 2021-12-20 12:40:38
+ * @LastEditTime: 2021-12-20 15:15:07
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \react-ui-2\.umirc.ts
@@ -13,8 +13,11 @@ export default defineConfig({
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  description:'哈哈哈',
+  description: '哈哈哈',
   outputPath: 'docs-dist',
+  alias: {
+    '@demo': '../../demo',
+  },
   // mode: 'site',
   // more config: https://d.umijs.org/config
 });
