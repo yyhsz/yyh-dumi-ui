4dba310fa51a00ac09cc39ee292de5c7a467b12b
余银海 <591985864@qq.com>
2021-12-20 16:54:46 +0800

    chore: 组件库打包包含less；对umi配置文件添加描述

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,3 +1,12 @@
+/*
+ * @Author: your name
+ * @Date: 2021-12-20 10:01:07
+ * @LastEditTime: 2021-12-20 16:42:20
+ * @LastEditors: Please set LastEditors
+ * @Description: 组件库的打包配置
+ * @FilePath: \react-ui-2\.fatherrc.ts
+ */
 export default {
   esm: 'babel',
+  lessInBabelMode: true,
 };
--- .umirc.ts
+++ .umirc.ts
@@ -1,9 +1,9 @@
 /*
  * @Author: your name
  * @Date: 2021-12-20 10:01:07
- * @LastEditTime: 2021-12-20 15:15:07
+ * @LastEditTime: 2021-12-20 16:42:30
  * @LastEditors: Please set LastEditors
- * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @Description: 文档的配置
  * @FilePath: \react-ui-2\.umirc.ts
  */
 import { defineConfig } from 'dumi';
