c0f8cb27a15d8e754b14998b621378c8bb2e90a8
余银海 <591985864@qq.com>
2021-12-20 16:56:20 +0800

    feat: 添加全局样式文件

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 /*
  * @Author: your name
  * @Date: 2021-12-20 10:01:08
- * @LastEditTime: 2021-12-20 14:38:01
+ * @LastEditTime: 2021-12-20 16:41:15
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \react-ui-2\src\index.ts
  */
+import './index.less';
+
 export { default as Foo } from './Button';
 export { default as Modal } from './Modal';
