1f0b386cff8854f89459d51c80ecccd2b9e4ba62
余银海 <591985864@qq.com>
2021-12-21 13:07:52 +0800

    feat: 添加iconfont

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \react-ui-2\src\index.ts
  */
+import './iconfont.js';
 import './index.less';
 
 export { default as Foo } from './Button';
