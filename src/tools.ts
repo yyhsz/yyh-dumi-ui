4c2c63b69b151a76438a32752b03f2c23337b25a
余银海 <591985864@qq.com>
2022-01-04 10:21:04 +0800

    refactor: className函数重构

--- src/tools.ts
+++ src/tools.ts
@@ -1,6 +1,26 @@
+interface classNameSuffix {
+  [key: string]: boolean;
+}
+
 const classNameGenerator = (prefix: string) => {
-  return (suffix?: string) => {
-    return ['yyh', prefix, suffix].filter(Boolean).join('-');
+  const str = `yyh-${prefix}`;
+  return (...suffix: Array<string | classNameSuffix>) => {
+    const suffixArr = suffix
+      .map((ele) => {
+        if (typeof ele !== 'string') {
+          // ['moday','asdfasdf']
+          return Object.entries(ele)
+            .filter((arr) => arr[1])
+            .map((arr) => arr[0]);
+        }
+        return ele;
+      })
+      .flat();
+    //without flat: ['header',['content','sider']] use flat:['header','content','sider']
+
+    return suffixArr.map((suffix) => (suffix ? `${str}-${suffix}` : str)).join(' ');
+
+    // return ['yyh', prefix, suffix].filter(Boolean).join('-');
   };
 };
 
